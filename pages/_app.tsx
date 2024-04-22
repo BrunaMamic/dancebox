1660777e01b3482c7bdc96780a40ad88f7bee12e
BrunaMamic <brunamamic00@gmail.com>
2024-04-22 16:22:07 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/index.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
